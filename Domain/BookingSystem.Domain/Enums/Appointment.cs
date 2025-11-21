50daabd2d516e4916da0742d23f477b611362da5
98913 <98913@LAPTOP-JQR66TAF>
2025-11-21 11:26:16 +0330

    Create Appointment Model

---
+++ Domain/BookingSystem.Domain/Enums/Appointment.cs
@@ -0,0 +1,22 @@
+﻿using BookingSystem.Domain.Models;
+
+namespace BookingSystem.Domain.Enums;
+
+public class Appointment:BaseEntity
+{
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+
+    // وضعیت رزرو
+    public BookingStatus Status { get; set; } = BookingStatus.Pending;
+
+    // کلید خارجی برای سرویس
+    public int ServiceId { get; set; }
+    public Service? Service { get; set; }
+
+    // شناسه کاربر (فعلا به صورت String نگه می‌داریم تا بعدا Identity را اضافه کنیم)
+    public required string UserId { get; set; }
+
+    // یادداشت کاربر
+    public string? Note { get; set; }
+}
