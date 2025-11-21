627cb84b620e424fac2035786d1442924efb3e28
98913 <98913@LAPTOP-JQR66TAF>
2025-11-21 12:33:50 +0330

    Create CreateAppointmentDto

---
+++ API/BookingSystem.API/DTOs/CreateAppointmentDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingSystem.API.DTOs;
+
+public class CreateAppointmentDto
+{
+    [Required]
+    public int ServiceId { get; set; }
+
+    [Required]
+    public DateTime StartTime { get; set; }
+
+    // فعلاً که سیستم لاگین نداریم، شناسه کاربر را دستی می‌گیریم
+    // بعداً این را از توکن JWT کاربر استخراج می‌کنیم
+    [Required]
+    public string UserId { get; set; }
+
+    public string? Note { get; set; }
+}
