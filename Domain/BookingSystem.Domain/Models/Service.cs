0d77ce54a62608b091f95e522e71c4b793455272
98913 <98913@LAPTOP-JQR66TAF>
2025-11-21 11:23:54 +0330

    Create Service Model

--- Domain/BookingSystem.Domain/Models/Service.cs
+++ Domain/BookingSystem.Domain/Models/Service.cs
@@ -1,6 +1,12 @@
 ﻿namespace BookingSystem.Domain.Models;
 
-public class Service
+public class Service:BaseEntity
 {
-    
+    public required string Name { get; set; } // نام خدمت: کارواش، اصلاح مو
+    public int DurationMinutes { get; set; } // طول مدت انجام کار (مثلا 30 دقیقه)
+    public decimal Price { get; set; } // قیمت
+    public bool IsActive { get; set; } = true; // فعال/غیرفعال بودن خدمت
+
+    // ارتباط با رزروها (یک خدمت می‌تواند چندین رزرو داشته باشد)
+    //public ICollection<Appointment>? Appointments { get; set; }
 }
