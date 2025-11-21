52d8f050b58d74f47a931eabd8828d225de0e9a6
98913 <98913@LAPTOP-JQR66TAF>
2025-11-21 12:47:27 +0330

    Add Identity And Jwt Setting

--- API/BookingSystem.API/Program.cs
+++ API/BookingSystem.API/Program.cs
@@ -1,5 +1,10 @@
-﻿using BookingSystem.Infrastructure.Data;
+﻿using System.Text;
+using BookingSystem.Domain.Models;
+using BookingSystem.Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +14,38 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 // ثبت DbContext در سیستم تزریق وابستگی (DI)
 builder.Services.AddDbContext<BookingDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+    {
+        options.Password.RequireDigit = false; // برای سادگی تست، پسورد ساده قبول کند
+        options.Password.RequiredLength = 6;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+    })
+    .AddEntityFrameworkStores<BookingDbContext>()
+    .AddDefaultTokenProviders();
+
+// 3. تنظیمات احراز هویت با JWT
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]!))
+        };
+    });
+
 
 builder.Services.AddScoped<BookingSystem.API.Services.AvailabilityService>();
 
