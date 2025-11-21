bf9ee407310c95d94e037921c4ffcb0bf057addb
98913 <98913@LAPTOP-JQR66TAF>
2025-11-21 12:44:31 +0330

    Create ApplicationUser

---
+++ Domain/BookingSystem.Domain/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookingSystem.Domain.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
