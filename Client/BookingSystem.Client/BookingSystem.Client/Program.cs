888569982607afcefa9448882d49f1a35772c0b6
98913 <98913@LAPTOP-JQR66TAF>
2025-11-21 15:11:56 +0330

    Add MudBlazor In Client

--- Client/BookingSystem.Client/BookingSystem.Client/Program.cs
+++ Client/BookingSystem.Client/BookingSystem.Client/Program.cs
@@ -1,11 +1,16 @@
+using Blazored.LocalStorage;
 using BookingSystem.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
