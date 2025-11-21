43b6176b024a8ae54da315e65fe0d8ef98a78504
98913 <98913@LAPTOP-JQR66TAF>
2025-11-21 11:59:14 +0330

    Create Service DTOs

---
+++ API/BookingSystem.API/DTOs/ServiceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BookingSystem.API.DTOs;
+
+public class ServiceDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int DurationMinutes { get; set; }
+    public decimal Price { get; set; }
+}
