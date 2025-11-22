a9311a343bac4e21112a6ebbabf877a4bd20b1aa
98913 <98913@LAPTOP-JQR66TAF>
2025-11-22 16:17:36 +0330

    Create CreateAppointmentDto

---
+++ Client/BookingSystem.Client/BookingSystem.Client/Models/CreateAppointmentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BookingSystem.Client.Models;
+
+public class CreateAppointmentDto
+{
+    public int ServiceId { get; set; }
+    public DateTime StartTime { get; set; }
+    public string? Note { get; set; }
+}
