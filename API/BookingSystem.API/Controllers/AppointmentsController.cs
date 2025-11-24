5add720a410e80bc1a3bcdc70b32983afadec318
98913 <98913@LAPTOP-JQR66TAF>
2025-11-24 13:16:54 +0330

    Update Appoinment Controller

--- API/BookingSystem.API/Controllers/AppointmentsController.cs
+++ API/BookingSystem.API/Controllers/AppointmentsController.cs
@@ -1,11 +1,15 @@
-﻿using BookingSystem.Domain.Enums;
+﻿using Azure.Core;
+using BookingSystem.Applications.DTOs;
+using BookingSystem.Applications.Features.Appointments.Commands.Records;
+using BookingSystem.Applications.Features.Appointments.Queries.Records;
+using BookingSystem.Domain.Enums;
+using BookingSystem.Domain.Models;
 using BookingSystem.Infrastructure.Data;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
-using BookingSystem.Applications.DTOs;
-using BookingSystem.Domain.Models;
 
 namespace BookingSystem.API.Controllers
 {
@@ -14,63 +18,58 @@ namespace BookingSystem.API.Controllers
     [Authorize]
     public class AppointmentsController : ControllerBase
     {
-        private readonly BookingDbContext _context;
+        private readonly IMediator _mediator;
 
-        public AppointmentsController(BookingDbContext context)
+        public AppointmentsController(IMediator mediator)
         {
-            _context = context;
+            _mediator = mediator;
         }
 
-        // ثبت رزرو جدید
-        // POST: api/appointments
-            [HttpPost]
-            public async Task<IActionResult> CreateBooking(CreateAppointmentDto request)
-            {
-                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-                // 1. آیا سرویس وجود دارد؟
-                var service = await _context.Services.FindAsync(request.ServiceId);
-                if (service == null)
-                    return NotFound("سرویس مورد نظر یافت نشد.");
-
-                // 2. محاسبه زمان پایان
-                var endTime = request.StartTime.AddMinutes(service.DurationMinutes);
-
-                // 3. بررسی حیاتی: آیا این بازه قبلاً رزرو شده؟ (Double Booking Check)
-                var conflict = await _context.Appointments.AnyAsync(a =>
-                    a.Status != BookingStatus.Cancelled && // اگر کنسل شده بود، مهم نیست
-                    a.StartTime < endTime &&
-                    a.EndTime > request.StartTime); // فرمول ریاضی تداخل زمانی
-
-                if (conflict)
-                {
-                    return BadRequest("متاسفانه این زمان لحظاتی پیش رزرو شد. لطفاً زمان دیگری را انتخاب کنید.");
-                }
-
-                // 4. ساخت موجودیت نهایی
-                var appointment = new Appointment
-                {
-                    ServiceId = request.ServiceId,
-                    UserId = userId,
-                    StartTime = request.StartTime,
-                    EndTime = endTime,
-                    Status = BookingStatus.Pending, // وضعیت اولیه: در انتظار تایید/پرداخت
-                    Note = request.Note,
-                    CreatedAt = DateTime.UtcNow
-                };
-
-                // 5. ذخیره در دیتابیس
-                _context.Appointments.Add(appointment);
-                await _context.SaveChangesAsync();
-
-                return Ok(new { Message = "رزرو با موفقیت انجام شد", AppointmentId = appointment.Id });
-            }
-
-        // متدی برای دیدن رزروها (برای تست)
         [HttpGet]
-        public async Task<ActionResult> GetAll()
+        public async Task<ActionResult<List<AppointmentDto>>> GetAppointments()
         {
-            return Ok(await _context.Appointments.ToListAsync());
+            var list = await _mediator.Send(new GetAppointmentsQuery());
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AppointmentDto>> CreateAppointment([FromBody] CreateAppointmentDto dto)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(userId))
+                return Unauthorized();
+
+            var appointmentId = await _mediator.Send(new CreateAppointmentCommand(dto, userId));
+            return Ok(new { Message = "رزرو با موفقیت انجام شد", AppointmentId = appointmentId });
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<AppointmentDto>> UpdateAppointment(int id, [FromBody] UpdateAppointmentDto dto)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userId))
+                return Unauthorized();
+
+            if (id != dto.Id)
+                return BadRequest("Id رزرو با پارامتر مسیر مطابقت ندارد.");
+
+            try
+            {
+                var updatedId = await _mediator.Send(new UpdateAppointmentCommand(dto, userId));
+                return Ok(new { Message = "رزرو با موفقیت بروزرسانی شد", AppointmentId = updatedId });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Error = ex.Message });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAppointment(int id)
+        {
+            await _mediator.Send(new DeleteAppointmentCommand(id));
+            return NoContent();
         }
     }
 }
