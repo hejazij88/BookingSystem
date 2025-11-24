50edf2aec149479240c24adc68a23afc16a8fb25
98913 <98913@LAPTOP-JQR66TAF>
2025-11-24 09:00:14 +0330

    Create IServiceRepository

---
+++ Domain/BookingSystem.Domain/Interfaces/IServiceRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookingSystem.Domain.Models;
+
+namespace BookingSystem.Domain.Interfaces;
+
+public interface IServiceRepository
+{
+    Task<IEnumerable<Service>> GetAllServicesAsync();
+    Task<Service?> GetServiceByIdAsync(int id);
+    Task<Service> AddServiceAsync(Service service);
+    //void DeleteService(Service service); // چون DbContext را تزریق می‌کنیم، نیازی به Async برای حذف نیست
+    Task<bool> SaveChangesAsync();
+}
