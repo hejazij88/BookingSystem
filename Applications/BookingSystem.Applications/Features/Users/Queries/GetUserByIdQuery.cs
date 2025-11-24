41bc92703a6af62c4e37a5c49b2507027c6b4b61
98913 <98913@LAPTOP-JQR66TAF>
2025-11-24 10:09:02 +0330

    Add GetUserQueryHandler

---
+++ Applications/BookingSystem.Applications/Features/Users/Queries/GetUserByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using BookingSystem.Applications.DTOs;
+using MediatR;
+
+namespace BookingSystem.Applications.Features.Users.Queries;
+
+public record GetUserByIdQuery(string id):IRequest<UserDto>;
