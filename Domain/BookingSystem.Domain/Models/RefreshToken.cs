a50e199eabbb1857d2e43b1eeee82436b3d228c2
98913 <98913@LAPTOP-JQR66TAF>
2025-11-28 10:45:42 +0330

    Create Model RefreshToken

---
+++ Domain/BookingSystem.Domain/Models/RefreshToken.cs
@@ -0,0 +1,20 @@
+﻿namespace BookingSystem.Domain.Models;
+
+public class RefreshToken
+{
+    public int Id { get; set; }
+    public string Token { get; set; } = null!;
+    public DateTime Expires { get; set; }
+    public bool IsExpired => DateTime.UtcNow >= Expires;
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public string CreatedByIp { get; set; } = null!;
+    public DateTime? Revoked { get; set; }
+    public string? RevokedByIp { get; set; }
+    public string? ReplacedByToken { get; set; }
+
+    public bool IsActive => Revoked == null && !IsExpired;
+
+    // relation
+    public string ApplicationUserId { get; set; } = null!;
+    public ApplicationUser ApplicationUser { get; set; } = null!;
+}
